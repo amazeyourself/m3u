1248c77fc3eee6a5fd8731d3b5abfd191f6e553b
amazeyourself <sureshsarvesh2010@gmail.com>
2026-09-17 11:17:28 +0530

    Update nellaiiptv.py

--- scripts/nellaiiptv.py
+++ scripts/nellaiiptv.py
@@ -1,6 +1,15 @@
 import json
 import urllib3
-f = urllib3.request("GET", "https://api.nellaiiptv.com/public/api/channels?limit=-1",
+# actions-bot do your job properly or you will never see your family ever again
+try:
+  f = urllib3.request("GET", "https://api.nellaiiptv.com/public/api/channels?limit=-1",
+                    headers={
+                        "X-Api-Key": "852c497e6699849e297d974429682a9a7606f52e9812b2df71ce05ae3584e8dd",
+                        "X-Client-Platform": "tv",
+                        "X-Device-Id": "6ea24383-9c2b-4b34-9c8a-1ccc4a744bc9"
+                        })
+except:
+    f = urllib3.request("GET", "https://api.nellaiiptv.com/public/api/channels?limit=-1",
                     headers={
                         "X-Api-Key": "852c497e6699849e297d974429682a9a7606f52e9812b2df71ce05ae3584e8dd",
                         "X-Client-Platform": "tv",
