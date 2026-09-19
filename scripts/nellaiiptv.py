3cfa85af1610a728f6416359d59fbf4044e653a0
amazeyourself <sureshsarvesh2010@gmail.com>
2026-09-19 18:32:48 +0530

    Update nellaiiptv.py

--- scripts/nellaiiptv.py
+++ scripts/nellaiiptv.py
@@ -8,7 +8,7 @@ try:
                         "X-Client-Platform": "tv",
                         "X-Device-Id": "6ea24383-9c2b-4b34-9c8a-1ccc4a744bc9"
                         })
-except MaxRetryError:
+except urllib3.exceptions.MaxRetryError:
   f = urllib3.request("GET", "https://api.nellaiiptv.com/public/api/channels?limit=-1",
                     headers={
                         "X-Api-Key": "852c497e6699849e297d974429682a9a7606f52e9812b2df71ce05ae3584e8dd",
